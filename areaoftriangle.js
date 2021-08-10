9055b569a174eece0e0419c2b0d4bc049ece1d10
Anish-JS <anishjakka@gmail.com>
2021-08-10 20:03:11 +0530

    styling
    tried different styling

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -6,7 +6,7 @@ const output = document.querySelector("#output");
 const calculateArea = () => {
   let Area = 0.5 * Number(height.value) * Number(base.value);
 
-  output.innerText = "Area of the triangle is " + Area;
+  output.innerText = "Area of the triangle is " + Area + "cm^2";
 };
 
 AreaBtn.addEventListener("click", calculateArea);
